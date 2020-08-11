fc06196dd973944cf10ffe3c49d1b8f71fc527bc
Andrey Ovcharov <andrew.ovcharov@gmail.com>
2020-08-11 14:56:40 +0200

    Fixed ambiguous conversion

--- src/ctap/makecredential.cpp
+++ src/ctap/makecredential.cpp
@@ -60,7 +60,7 @@ namespace FIDO2
                         return CTAP2_ERR_MISSING_PARAMETER;
                     }
 
-                    int8_t alg = cborAlg;
+                    int8_t alg = (char)cborAlg;
                     request->algorithms.push_back(alg);
                 }
 
